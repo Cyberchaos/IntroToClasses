9edcf28d9b574af9a356d061d35d00960049a189
Cyberchaos <dualgunner42@gmail.com>
2020-08-18 10:55:53 +0200

    Add project files.

---
+++ IntroToClasses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IntroToClasses
+{
+    class Program
+    {
+       
+        class Fairy
+        {
+
+        }
+        class Corgi
+        {
+            public string name;
+            private int width, height, length;
+            private Fairy rider;
+
+            public string Borf()
+            {
+                return name +  ": BORK!";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] arr;
+            arr = new string[8];
+
+            Corgi clara= new Corgi();
+            Corgi basil = new Corgi();
+
+            clara.name = "Clara";
+            basil.name = "Basil";
+
+            Console.WriteLine(clara.Borf());
+            Console.WriteLine(basil.Borf());
+
+
+        }
+    }
+}
